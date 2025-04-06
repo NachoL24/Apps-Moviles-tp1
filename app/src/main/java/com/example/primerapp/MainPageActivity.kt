9404d333441f121ee2c512704c756ab3dab9c668
MartinEliseche <tinchoeliseche@gmail.com>
2025-04-05 21:19:54 -0300

    gradle dependencies extended

--- app/src/main/java/com/example/primerapp/MainPageActivity.kt
+++ app/src/main/java/com/example/primerapp/MainPageActivity.kt
@@ -1,4 +1,30 @@
 package com.example.primerapp
 
-class MainPageActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.core.view.isVisible
+
+class MainPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainpage)
+
+        val cbOther = findViewById<CheckBox>(R.id.cbOther)
+        val etOtherSpecify = findViewById<EditText>(R.id.etOtherSpecify)
+        val btnAccept = findViewById<Button>(R.id.btnAccept)
+
+        cbOther.setOnCheckedChangeListener { _, isChecked ->
+            etOtherSpecify.visibility = if (isChecked) View.VISIBLE else View.GONE
+        }
+
+        btnAccept.setOnClickListener {
+            if (cbOther.isChecked && etOtherSpecify.text.isNullOrEmpty()) {
+                etOtherSpecify.error = "Por favor especifica al menos una preferencia"
+            } else {
+                Toast.makeText(this, "Preferencias guardadas", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
