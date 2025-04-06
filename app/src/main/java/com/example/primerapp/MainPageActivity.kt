6c059bf1bbc86116414c29ae30182a240f102957
MartinEliseche <tinchoeliseche@gmail.com>
2025-04-06 16:20:17 -0300

    logic finished

--- app/src/main/java/com/example/primerapp/MainPageActivity.kt
+++ app/src/main/java/com/example/primerapp/MainPageActivity.kt
@@ -1,7 +1,9 @@
 package com.example.primerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.primerapp.databinding.MainpageBinding
 
 class MainPageActivity : AppCompatActivity() {
@@ -13,8 +15,40 @@ class MainPageActivity : AppCompatActivity() {
         binding = MainpageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Mensaje de bienvenida para Juan Torres
         val userName = "Juan Torres"
         val welcome = getString(R.string.welcome_message, userName)
         binding.tvWelcomeTitle.text = welcome
+
+        binding.cbOther.setOnCheckedChangeListener { _, isChecked ->
+            binding.etOtherSpecify.visibility = if (isChecked) View.VISIBLE else View.GONE
+        }
+
+
+        // Validacion de Pickers
+        binding.btnAccept.setOnClickListener {
+
+            // Validacion de Plataforma
+            val selectedId = binding.rgPlatform.checkedRadioButtonId
+            if (selectedId == -1) {
+                Toast.makeText(this, "Debe seleccionar una plataforma", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+            // Validacion de Preferencias
+            val isAnyPreferenceChecked =
+                binding.cbProgramming.isChecked ||
+                        binding.cbNetworks.isChecked ||
+                        binding.cbHardware.isChecked ||
+                        binding.cbSecurity.isChecked ||
+                        binding.cbOther.isChecked
+
+            if (!isAnyPreferenceChecked) {
+                Toast.makeText(this, "Debe seleccionar al menos una preferencia", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+        }
     }
 }
